554505e97eef4914dad7181d8b5c68ccfecf6b4c
flypizza <flypizza2@gmail.com>
2024-11-30 19:37:35 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import GoogleLoginButton from "./components/GoogleLoginButton";
 export default function Home() {
   return (
     <div className="bg-white h-screen w-screen text-black flex flex-col justify-center items-center">
-      <h1>Next Js fire base fire store</h1>
+      <h1>Next Js fire base fire store _ minho</h1>
       <GoogleLoginButton></GoogleLoginButton>
 
     </div>
